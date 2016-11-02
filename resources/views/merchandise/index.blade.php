f30b88231e0bc1260ab595362f7415d22b4b70fe
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-11-02 14:46:01 -0500

    merch

--- resources/views/merchandise/index.blade.php
+++ resources/views/merchandise/index.blade.php
@@ -3,9 +3,9 @@
 <div class="container text-center">
 <div class="merchItm">
 <a target="blank" href="http://www.cdbaby.com/cd/kennykensthebrownbottleb"><img  class="merch" src="/img/cover.jpg"><br>Audio Download</a><br/><h3>Album Hard Copy $10.00</h3></div>
-<div class="merchItm"><img class="merch" src="/img/tshirt.png"><br/><h2>T-Shirt $10.00</h2></div>
+{{-- <div class="merchItm"><img class="merch" src="/img/tshirt.png"><br/><h2>T-Shirt $10.00</h2></div>
 <div class="merchItm"><img class="merch" src="/img/huggie.png"><h2>Cold Beer Koozie $3.00</h2></div>
-<div class="merchItm"><img class="merch" src="/img/sticker.png"><h2>Sticker $1.00</h2></div>
+<div class="merchItm"><img class="merch" src="/img/sticker.png"><h2>Sticker $1.00</h2></div> --}}
 
 </div>
 
