8ed5aff48dac2a8eda6264a26445038c899c7123
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-11-02 16:49:47 -0500

    merchss

--- resources/views/merchandise/index.blade.php
+++ resources/views/merchandise/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 @section('content')
 <div class="container text-center">
+<h1>Fan Contraband</h1>
 <div class="merchItm">
 <a target="blank" href="http://www.cdbaby.com/cd/kennykensthebrownbottleb"><img  class="merch" src="/img/cover.jpg"><br>Audio Download</a><br/><h3>Album Hard Copy $10.00</h3></div>
-{{-- <div class="merchItm"><img class="merch" src="/img/tshirt.png"><br/><h2>T-Shirt $10.00</h2></div>
-<div class="merchItm"><img class="merch" src="/img/huggie.png"><h2>Cold Beer Koozie $3.00</h2></div>
+<div class="merchItm"><img class="merch" src="/img/tshirt_black.png"><img class="merch" src="/img/tshirt_white.png"><p><h2>T-Shirt</h2><h3>S, M, L, XL $10.00</h3><h3>XXL, XXXL $13.00</h3></p></div>
+{{-- <div class="merchItm"><img class="merch" src="/img/huggie.png"><h2>Cold Beer Koozie $3.00</h2></div>
 <div class="merchItm"><img class="merch" src="/img/sticker.png"><h2>Sticker $1.00</h2></div> --}}
 
 </div>
