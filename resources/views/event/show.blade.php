c8ff1a64f3b9e295ee48a2ca28d93cd4f52c2410
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-11-09 11:05:56 -0600

    storage

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -6,7 +6,7 @@
 <p><span style="color: #337ab7"><u>Start Date and Time:</u></span><br/>
 {{ date('F d, Y', strtotime($event->start_date)) }}<br/>{{ date('h:i:s a', strtotime($event->start_time)) }}</p>
 </div>
-<div class="col-md-6 col-md-offset-3" style="background-color: #000; margin-top: 50px">
+<div class="col-md-6 col-md-offset-3" style="background-color: #000; margin-top: 50px; min-height: 50%">
 <h2>{!!$event->body!!}</h2>
 @if(\Auth::check())
 		<a style="float: right" class="btn btn-secondary" href="{{url('/event/'.$event->id.'/edit')}}">edit</a>
