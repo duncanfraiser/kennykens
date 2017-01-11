63105504e7f2d9f8ed44b751fd6862e2d2748e25
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-01-11 10:58:09 -0600

    test

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Book;
-
+//test commint DELETE
 class BookController extends Controller
 {
         public function __construct(){
